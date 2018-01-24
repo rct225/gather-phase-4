1d26718f4028917e70a97328bd2c7078d02f16de
Robert Tuck <robtuck@gmail.com>
2018-01-24 12:47:08 -0500

    added user test for clicking on newly created item from main page

--- test/features/user-visits-create-test.js
+++ test/features/user-visits-create-test.js
@@ -3,16 +3,18 @@ const {buildItemObject} = require('../test-utils');
 
 describe('User visits the create page', () => {
     describe('posts a new item', () => {
-      it('and is rendered', () => {
+      it('and clicks on the created item', () => {
         const itemToCreate = buildItemObject();
         browser.url('/items/create');
         browser.setValue('#title-input', itemToCreate.title);
         browser.setValue('#description-input', itemToCreate.description);
         browser.setValue('#imageUrl-input', itemToCreate.imageUrl);
         browser.click('#submit-button');
-        assert.include(browser.getText('body'), itemToCreate.title);
-        assert.include(browser.getAttribute('body img', 'src'), itemToCreate.imageUrl);
+        const xpath = `//div[@class="item-title"]/p[text() = "${itemToCreate.title}"]/ancestor::div[@class="item-card"]/a`;
+        //console.log(xpath);
+        browser.click(xpath);
+        //console.log(browser.getHTML('body'));
+        assert.include(browser.getText('body'), itemToCreate.description)
       });
     });
 });
-
